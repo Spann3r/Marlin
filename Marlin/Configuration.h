bb1318a7c8c86139f2e163e93aff6f297c1663dc
Ian Patterson <ian.p.patterson@gmail.com>
2020-09-13 13:16:44 +0100

    Tuning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -489,9 +489,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 27.63
+  #define DEFAULT_Ki 2.69
+  #define DEFAULT_Kd 70.88
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
@@ -527,7 +527,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-#define PIDTEMPBED
+//#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -751,7 +751,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 95.88 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -997,7 +997,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44.4, -10, -3 }
+#define NOZZLE_TO_PROBE_OFFSET { -44.4, -10, -2 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
